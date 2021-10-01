053f01cc81eecbb87fd54f32eb95286c9cae54e0
alvian21 <alvian.ardhiansyah@gmail.com>
2021-10-01 10:26:22 +0700

    revisi

--- database/seeds/BahanSeeder.php
+++ database/seeds/BahanSeeder.php
@@ -16,15 +16,15 @@ class BahanSeeder extends Seeder
     public function run()
     {
         $faker = Factory::create();
-        $status =['bahan masuk', 'bahan keluar'];
-        $vendor =['Triple A', 'J. Developer', 'Kaos Polos Surabaya', 'Chief Garment', 'Quax Kain'];
-        $warna =['blue', 'red', 'maroon', 'green', 'yellow'];
-        $nama =['kaos polos', 'polo bersaku', 'kaos polo bersaku', 'polo polos'];
-        $jenis =['Cotton Combed 38s', 'Cotton Combed 28s', 'Cotton Combed 23s', 'Cotton Combed 14s'];
+        $status = ['bahan masuk', 'bahan keluar'];
+        $vendor = ['Triple A', 'J. Developer', 'Kaos Polos Surabaya', 'Chief Garment', 'Quax Kain'];
+        $warna = ['blue', 'red', 'maroon', 'green', 'yellow'];
+        $nama = ['kaos polos', 'polo bersaku', 'kaos polo bersaku', 'polo polos'];
+        $jenis = ['Cotton Combed 38s', 'Cotton Combed 28s', 'Cotton Combed 23s', 'Cotton Combed 14s'];
         for ($i = 0; $i <= 250; $i++) {
             $bahan = new Bahan();
-            $bahan->detail_sub_kategori_id = Arr::random([1,2]);
-            $bahan->kode_transaksi = $this->getKode();
+            $bahan->detail_sub_kategori_id = Arr::random([1, 2]);
+            $bahan->kode_transaksi = $this->getKode($i);
             $bahan->kode_bahan = '#B00' . $i;
             $bahan->no_surat = '#SJM00' . $i;
             $bahan->status = Arr::random($status);
@@ -37,22 +37,16 @@ class BahanSeeder extends Seeder
             $bahan->warna = Arr::random($warna);
             $bahan->jenis_bahan = Arr::random($jenis);
             $bahan->vendor = Arr::random($vendor);
-            $bahan->panjang_bahan = $faker->numberBetween(40,200);
+            $bahan->panjang_bahan = $faker->numberBetween(40, 200);
             $bahan->save();
-
         }
     }
 
-    public function getKode()
+    public function getKode($i)
     {
         $kode_transaksi = Bahan::select('kode_transaksi')->whereNotNull('kode_transaksi')->orderBy('created_at', 'DESC')->first();
-        if (!$kode_transaksi) {
-            $datakode = "TR-" . date('Ymd') . "1";
-        } else {
-            $last = substr($kode_transaksi->kode_transaksi, -1);
-            $jumlah = $last + 1;
-            $datakode = "TR-" . date('Ymd') . $jumlah;
-        }
+        $jumlah = $i + 1;
+        $datakode = "TR-" . date('Ymd') . $jumlah;
 
         return $datakode;
     }
