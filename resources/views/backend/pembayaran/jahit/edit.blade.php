b38074a0cfab34d1813b5cfbaa6208ae2350917f
alvian21 <alvian.ardhiansyah@gmail.com>
2021-10-08 12:39:43 +0700

    revisi

--- resources/views/backend/pembayaran/jahit/edit.blade.php
+++ resources/views/backend/pembayaran/jahit/edit.blade.php
@@ -454,6 +454,8 @@
                         }
                     }else if(nominal2 > 0 && nominal > 0){
                         var total = parseInt(nominal) + parseInt(nominal2)
+                        alert(total)
+                        alert(hasil)
                         if(parseInt(total) <= parseInt(hasil) ){
                             $('#dataalert').show()
                             $('#dataalert').text('Nominal pembayaran harus kurang dari sama dengan sisa bayar')
