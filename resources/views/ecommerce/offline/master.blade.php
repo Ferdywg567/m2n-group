bcb6883c3be1dfc71caec19d35b9e62e17a6ba40
Your <alvian.ardhiansyah@gmail.com>
2021-12-17 10:21:42 +0700

    revisi

--- resources/views/ecommerce/offline/master.blade.php
+++ resources/views/ecommerce/offline/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>@yield('title') &dash; Garmen</title>
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    {{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- General CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
