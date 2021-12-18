0ccdc1fadaadf51f1f54137f229f7e3f2fdf536a
Your <alvian.ardhiansyah@gmail.com>
2021-12-18 13:24:03 +0700

    revisi

--- app/Http/Controllers/API/CariController.php
+++ app/Http/Controllers/API/CariController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\DetailProdukImage;
 use App\Produk;
 
 class CariController extends Controller
@@ -19,9 +20,35 @@ class CariController extends Controller
         if ($request->has('data')) {
             $data = $request->get('data');
             $produk = Produk::where('sub_kategori', 'like', '%' . $data . '%')->orwhere('kategori', 'like', '%' . $data . '%')->orwhere('detail_sub_kategori', 'like', '%' . $data . '%')->orwhere('nama_produk', 'like', '%' . $data . '%')->get();
+            $arr = [];
+            foreach ($produk as $key => $value) {
+                $gambar = '';
+                $argambar = [];
+                $detailgambar = DetailProdukImage::where('produk_id', $value->id)->first();
+                if ($detailgambar) {
+                    $gambar = asset('uploads/images/produk/' . $detailgambar->filename);
+                }
+
+                $detailgambarall = DetailProdukImage::where('produk_id', $value->id)->get();
+                if ($detailgambarall->isNotEmpty()) {
+                    foreach ($detailgambarall as $key => $row) {
+                        $y['gambar'] = asset('uploads/images/produk/' . $row->filename);
+                        array_push($argambar, $y);
+                    }
+                    $x['detail_gambar'] = $argambar;
+                } else {
+                    $x['detail_gambar'] = [];
+                }
+
+                $cv = json_decode(json_encode($value), true);
+                $x['gambar'] = $gambar;
+
+                $data = array_merge($x, $cv);
+                array_push($arr, $data);
+            }
             return response()->json([
                 'status' => true,
-                'data' => $produk,
+                'data' => $arr,
                 'code' => Response::HTTP_OK
             ]);
         }
