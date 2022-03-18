96952b62c71345bc388d16b98155284c362ec7df
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2022-03-18 14:06:04 +0700

    revisi

--- resources/views/ecommerce/frontend/main.blade.php
+++ resources/views/ecommerce/frontend/main.blade.php
@@ -11,7 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('ecommerce/assets/images/favicon.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/assets/img/M2N 1.png')}}">
 
     <!-- All CSS is here
 	============================================ -->
