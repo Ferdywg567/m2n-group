6d00346474acc25328ede7d219f6088c2ba56227
Alvian Ardhiansyah <alvian.ardhiansyah@gmail.com>
2022-03-31 22:53:24 +0700

    revisi

--- resources/views/ecommerce/admin/dashboard/index.blade.php
+++ resources/views/ecommerce/admin/dashboard/index.blade.php
@@ -62,8 +62,22 @@
                         datasets: [{
                             label: 'Pendapatan',
                             data: data,
-                            backgroundColor:color,
-                            borderColor:color,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
                             borderWidth: 1
                         }]
                     },
@@ -106,7 +120,7 @@
                             color_online.push(randomColorGenerator())
                         });
 
-                           setTimeout(() => {
+                        setTimeout(() => {
                             barChartData(total_online, tanggal_online, 'pendapatanOnline',
                                 color_online)
                         }, 1000);
